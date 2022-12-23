3c5def2e0971ba2cd5e563cfdb82823ae2d8884b
sonimohit481 <sonimohit481@gmail.com>
2022-12-23 16:21:10 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import './App.css';
+import { NavBar } from './Components/NavBar';
 function App() {
   return (
     <div className="App">
-      Mohit Soni
+      <NavBar />
     </div>
   );
 }
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+export const NavBar = () => {
+  return (
+    <div id="Header">
+      <div id="leftHeader">
+        <div>
+          <img src="assets/images/logomain.png" />
+        </div>
+        <div></div>
+      </div>
+      <div id="rightHeader"></div>
+    </div>
+  );
+};
